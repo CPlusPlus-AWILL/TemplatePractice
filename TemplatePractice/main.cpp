6efb0da995ba4e479d4d569a06467f612813e845
xiawuhao2013 <xiawuhao2011@foxmail.com>
2019-12-12 19:10:48 +0800

    feat: compare some template functions

--- TemplatePractice/main.cpp
+++ TemplatePractice/main.cpp
@@ -1,8 +1,44 @@
 ﻿#include <iostream>
 
+template<typename Tp>
+class Point
+{
+public:
+    Tp x;
+    Tp y;
+
+};
+
+template<typename Tp>
+static inline Tp Test(const Point<int>& pt);
+template<typename Tp>
+static inline Tp Test(const Point<double>& pt);
+
+template<>
+inline int Test<int>(const Point<int>& pt)
+{
+    return 0;
+}
+
+template<typename Tp>
+inline Tp Test<Tp>(const Point<double>& pt)
+{
+    return 0;
+}
+
+template<>
+inline double Test<double>(const Point<double>& pt)
+{
+    return 0;
+}
+
 int main()
 {
     std::cout << "Hello World!\n"; 
+    Point<int> a = { 1,1 };
+    Point<double> b = { 0,0 };
+    Test<int>(a);
+    Test<double>(b);
 
     return 0;
 }
